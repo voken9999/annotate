8d89a47eb72f503818bcba823f1be595b7acd865
voken9999 <voken2959@gmail.com>
2026-06-03 23:22:05 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import "./globals.css";
+import {
+  ToolProvider,
+} from "@/context/ToolContext";
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <ToolProvider>
+          {children}
+        </ToolProvider>
+      </body>
+    </html>
+  );
+}
